2fd85040ffb84664f3c9b94edf772d87bdf7e0be
Nathan Yang <112563109+rhit-yangnj@users.noreply.github.com>
2024-02-01 18:42:33 -0500

    gave application account sproc permissions

--- migrations/V1.14__Add application account as user.sql
+++ migrations/V1.14__Add application account as user.sql
@@ -2,7 +2,11 @@ CREATE USER [gameLibrary] FOR LOGIN [gameLibrary]
 GO
 ALTER ROLE [db_datareader] ADD MEMBER [gameLibrary]
 GO
-USE [GameLibraryDB]
-GO
 ALTER ROLE [db_datawriter] ADD MEMBER [gameLibrary]
+GO
+GRANT EXECUTE ON [dbo].[SearchGames] TO [gameLibrary] AS [dbo]
+GO
+GRANT EXECUTE ON [dbo].[getAllGames] TO [gameLibrary] AS [dbo]
+GO
+GRANT EXECUTE ON [dbo].[getPersonalGames] TO [gameLibrary] AS [dbo]
 GO
